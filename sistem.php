68eed0b453f7ada1fbcdb86b355701130a370bd1
Pramuja <42104207+Pramuja@users.noreply.github.com>
2019-01-09 11:16:25 +0700

    Update sistem.php

--- sistem.php
+++ sistem.php
@@ -1,5 +1,15 @@
 <?php
+    $name = $_POST['nama'];
+$email = $_POST['email'];
+
+if($name == "" || $email == "")
+{
+    header("location: tugas.php");
+}
+else
+{
     echo $_POST['nama'];
     echo "<br>";
     echo $_POST['email'];
+}
 ?>
